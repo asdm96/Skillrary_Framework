73e1eede3f568ccb4808154eb23512dcdd24253e
asd <asd@Adarsh>
2021-05-10 18:10:36 +0530

    skillrary framework

---
+++ com.skillrary/src/main/java/com/skillRary/GenericUtils/IPathConstant.java
@@ -0,0 +1,12 @@
+package com.skillRary.GenericUtils;
+/**
+ * This interface contains file path which is required in framework
+ * 
+ *
+ */
+public interface IPathConstant {
+	String PROPERTY_FILEPATH="";
+	String JSONFILEPATH="";
+	String EXCELPATH="";
+	String htmlPath="./extentReport"+JavaUtility.getCurrentSystemDate()+".html";
+}
